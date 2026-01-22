af5d30a2f268ee4a269b761ccc03999c15a4d72f
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-01-22 23:31:07 +0530

    update

--- Fundamentals/Calculator.java
+++ Fundamentals/Calculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // Take input from user till user does not press X or x
+        
         int ans = 0;
         while (true) {
             // take the operator as input

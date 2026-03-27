eb18f2e221f610916a2fb43deaf4070c0a59cfca
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-03-27 22:17:32 +0530

    HashMap

--- HashSetHashMap/basicset.java
+++ HashSetHashMap/basicset.java
@@ -5,11 +5,16 @@ public class basicset {
         HashSet<Integer> set = new HashSet<>();
         set.add(11);set.add(4); set.add(4); set.add(10);
         set.add(11);set.add(15);set.add(34);set.add(30);
-        System.out.println(set.size());
-        System.out.println(set.contains(34));
-        // set.remove(11);
-        System.out.println(set.size());
-        System.out.println(set);
+// iterate on set only with for each loop
+        for(int nums:set){
+            System.out.print(nums+" ");
+        }
+
+        // System.out.println(set.size());
+        // System.out.println(set.contains(34));
+        // // set.remove(11);
+        // System.out.println(set.size());
+        // System.out.println(set);
 
     }
 }
---
+++ HashSetHashMap/basicsmap.java
@@ -0,0 +1,19 @@
+package HashSetHashMap;
+import java.util.*;
+class basicsmap{
+    public static void main(String[] args){
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("Manish",20);map.put("Abhishek",21);
+        map.put("Ashish",19);map.put("Anubhav",22);
+        for(String key:map.keySet()){HAsh
+            System.out.println(key+" "+map.get(key));
+        }
+        // System.out.println(map+" "+map.size());
+        // System.out.println(map.remove("Ashish"));
+        // System.out.println(map+" "+map.size());
+        // System.out.println(map.get("Abhishek"));
+        // System.out.println(map.containsKey("Ashish"));
+
+
+    }
+}

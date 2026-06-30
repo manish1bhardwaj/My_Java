ab06b29e2babbd3f283707d2db496069124b63a8
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-06-30 23:52:49 +0530

    Rotate Array K times Optimized solution

---
+++ Class_Practice/RotateArrayReversal.java
@@ -0,0 +1,37 @@
+package Class_Practice;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class RotateArrayReversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+
+            l++;
+            r--;
+        }
+    }
+}
